e65704bb7db5d10bc7dd6e08d5ef03029a01f313
Asvin Thakur <asvinthakur07@gmail.com>
2026-01-05 19:08:04 +0530

    first backend project

---
+++ Server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const dotenv = require ('dotenv');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+const productRoutes = require('./routes/productRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+const {notFound, errorHandler} = require('./middleware/errorMiddleware');
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api/users', authRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/orders', orderRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server Running on port ${PORT}`));
